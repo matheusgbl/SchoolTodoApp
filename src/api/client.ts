6aa1506bbcea05edb73f65c45b0abac5d6264f0c
matheusgbl <matheusluiz.barbosa@outlook.com>
2025-07-03 20:15:42 -0300

    return metro.config.js

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Para Android, use o IP da sua máquina na rede local ou o IP especial 10.0.2.2
 // Para iOS, localhost funciona se o app estiver no simulador.
-const baseURL = 'http://localhost:3001'; // IP especial do emulador Android para o localhost da máquina
+const baseURL = 'http://10.0.2.2:3001'; // IP especial do emulador Android para o localhost da máquina
 
 const apiClient = axios.create({
   baseURL,
